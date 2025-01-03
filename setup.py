fdbadb428d1b061b4617e4dcb1f1978305d2de6b
Teruma Yamamoto <TRyamamototeruma@gmail.com>
2025-01-04 02:24:37 +0900

    first

--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@ setup(
     version='0.1.0',
     packages=[package_name],
     py_modules=[
-        'internet_speed_display.internet_speed_publisher'  # ファイル名を指定
+        'internet_speed_display.internet_speed_publisher'
     ],
     install_requires=['setuptools', 'speedtest-cli'],
     zip_safe=True,
-    maintainer='Your Name',
-    maintainer_email='your_email@example.com',
+    maintainer='Teruma yamamoto',
+    maintainer_email='TRyamamototeruma@gmail.com',
     description='A ROS 2 package to publish internet speed.',
     license='BSD 3-Clause',
     entry_points={
